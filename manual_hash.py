3d585da379695d021aa800826ee0f9bcd7e46f25
Ayaulym Idrisheva <157778099+IdAyaulym@users.noreply.github.com>
2025-01-25 11:21:00 +0600

    Update manual_hash.py

--- manual_hash.py
+++ manual_hash.py
@@ -1,9 +1,12 @@
 def manual_hash(data):
     hash_value = 0
     for char in data:
-        hash_value = (hash_value * 31 + ord(char)) % (2**32)
-    return hash_value
+        hash_value = (hash_value * 31 + ord(char)) % (2 ** 32)
+
+    hex_hash = hex(hash_value)[2:]
+    return hex_hash.zfill(8)
+
 
 if __name__ == "__main__":
+    print(manual_hash("1"))
     print(manual_hash("Мысалы"))
-
