784d3c322a1394cfbd36409b66af6f25ac38a000
Zhako1 <158266551+Zhako1@users.noreply.github.com>
2025-01-31 16:59:18 +0500

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -1,29 +1,37 @@
+import json
 from block import Block
+from utxo import UTXO
+from transaction import Transaction
 
 class Blockchain:
     def __init__(self):
-        self.chain = [Block("Генезис блогы")]
+        self.chain = [Block([])]
+        self.utxo = UTXO()
+        self.load_transactions()
 
-    def add_block(self, data):
-        previous_block = self.chain[-1]
-        new_block = Block(data, previous_block.hash)
-        self.chain.append(new_block)
+    def add_block(self, transaction):
+        if self.validate_transaction(transaction):
+            previous_block = self.chain[-1]
+            new_block = Block([transaction], previous_block.hash)
+            self.chain.append(new_block)
+            self.save_transactions()
+            return True
+        return False
 
-    def validate_chain(self):
-        for i in range(1, len(self.chain)):
-            current_block = self.chain[i]
-            previous_block = self.chain[i - 1]
+    def validate_transaction(self, transaction):
+        return self.utxo.create_utxo(transaction)
 
-            if current_block.previous_hash != previous_block.hash:
-                return False
-            if current_block.hash != current_block.calculate_hash():
-                return False
-        return True
+    def save_transactions(self):
+        with open('transactions.json', 'w') as f:
+            transactions = [tx.to_dict() for block in self.chain for tx in block.transactions]
+            json.dump(transactions, f)
 
-if __name__ == "__main__":
-    from block import Block
-    blockchain = Blockchain()
-    blockchain.add_block("Блок 1")
-    blockchain.add_block("Блок 2")
-    for block in blockchain.chain:
-        print(f"Блок: {block.hash}, Деректер: {block.data}")
+    def load_transactions(self):
+        try:
+            with open('transactions.json', 'r') as f:
+                transactions = json.load(f)
+                for tx_data in transactions:
+                    transaction = Transaction(tx_data['sender'], tx_data['recipient'], tx_data['amount'], tx_data['fee'])
+                    self.add_block(transaction)
+        except FileNotFoundError:
+            pass
