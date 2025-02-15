eea64f7689a2806a877c5f3732c094a1ae628aba
Zhako1 <158266551+Zhako1@users.noreply.github.com>
2025-02-15 10:24:28 +0500

    transaction.py

--- transaction.py
+++ transaction.py
@@ -17,7 +17,7 @@ class Transaction:
         transaction_data = f"{self.sender}{self.recipient}{self.amount}{self.fee}"
         data_to_hash = transaction_data + str(private_key[1])
         hashed_data = int(manual_hash(data_to_hash), 16) % private_key[1]  # Делаем число <= N
-        self.signature = hex(pow(hashed_data, private_key[0], private_key[1]))[2:]  # Подписываем
+        self.signature = hex(pow(hashed_data, private_key[0], private_key[1]))[2:]  
 
     def verify_signature(self, public_key):
         transaction_data = f"{self.sender}{self.recipient}{self.amount}{self.fee}"
@@ -26,10 +26,10 @@ class Transaction:
         decrypted_signature = pow(int(self.signature, 16), public_key[0], public_key[1])  # Расшифровка
 
         if decrypted_signature == expected_hash:
-            print("[VERIFY] ✅ Подпись верна.")
+            print("[VERIFY]  Подпись верна.")
             return True
         else:
-            print("[VERIFY] ❌ Подпись неверна.")
+            print("[VERIFY]  Подпись неверна.")
             return False
 
     def to_dict(self):
