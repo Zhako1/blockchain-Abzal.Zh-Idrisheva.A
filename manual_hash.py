45715a02fdddadb57c7e36256fc651ac0177b915
Zhako1 <158266551+Zhako1@users.noreply.github.com>
2025-02-15 10:12:04 +0500

    Update manual_hash.py 3

--- manual_hash.py
+++ manual_hash.py
@@ -4,8 +4,5 @@ def manual_hash(data):
         hash_value = (hash_value * 31 + ord(char)) % (2 ** 32)
 
     hex_hash = hex(hash_value)[2:]
-    return hex_hash.zfill(8)
+    return hex(hash_value)[2:].zfill(8)
 
-if __name__ == "__main__":
-    print(manual_hash("1"))
-    print(manual_hash("Мысалы"))
