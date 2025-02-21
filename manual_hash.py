04fb42b4f0352b6ed3a0192a3060e0a9be39c481
Zhako1 <158266551+Zhako1@users.noreply.github.com>
2025-02-21 19:25:13 +0500

    Update manual_hash.py

--- manual_hash.py
+++ manual_hash.py
@@ -2,7 +2,6 @@ def manual_hash(data):
     hash_value = 0
     for char in data:
         hash_value = (hash_value * 31 + ord(char)) % (2 ** 32)
-
-    hex_hash = hex(hash_value)[2:]
+        
     return hex(hash_value)[2:].zfill(8)
 
