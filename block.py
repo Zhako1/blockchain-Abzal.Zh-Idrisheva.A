610f04035d1b2f1f434116a42d0b9c52f8fe250a
Zhako1 <158266551+Zhako1@users.noreply.github.com>
2025-03-01 05:40:59 +0500

    Update block.py

--- block.py
+++ block.py
@@ -4,12 +4,13 @@ from transaction import Transaction
 from manual_hash import manual_hash
 
 class Block:
-    def __init__(self, transactions, previous_hash=''):
-        self.transactions = transactions
+    def __init__(self, transactions, previous_hash='', difficulty=2):
+        self.transactions = [Transaction(**tx) if isinstance(tx, dict) else tx for tx in transactions]
         self.timestamp = datetime.datetime.now()
         self.previous_hash = previous_hash
         self.merkle_root = self.calculate_merkle_root()
         self.hash = self.calculate_hash()
+        self.difficulty = difficulty
 
 
     def calculate_merkle_root(self):
@@ -22,10 +23,20 @@ class Block:
     def calculate_hash(self):
         block_data = f"{self.merkle_root}{self.timestamp}{self.previous_hash}"
         return manual_hash(block_data)
+        
+    def mine_block(self, difficulty, miner_address):
+        nonce = 0
+        while True:
+            block_data = f"{self.merkle_root}{self.timestamp}{self.previous_hash}{nonce}{miner_address}"
+            block_hash = manual_hash(block_data)  
+            if block_hash[:difficulty] == "0" * difficulty:  
+                self.hash = block_hash
+                return nonce
+            nonce += 1
 
     def to_dict(self):
         return {
-            "timestamp": self.timestamp.isoformat(),  # Преобразуем в строку
+            "timestamp": self.timestamp.isoformat(), 
             "previous_hash": self.previous_hash,
             "transactions": [tx.to_dict() for tx in self.transactions],
             "hash": self.hash
